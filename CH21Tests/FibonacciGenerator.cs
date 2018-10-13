5496226a183dfa5c056ffbe65ad97d47be60b7c5
d-german <facmet107@gmail.com>
2018-10-13 12:45:06 -0500

    made some changes

---
+++ CH21Tests/FibonacciGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace CH21Tests
+{
+    public class FibonacciGenerator
+    {
+        private IDictionary<int, int> _cache;
+
+        private FibonacciGenerator()
+        {
+        }
+
+        public List<int> Nums { get; private set; }
+
+        public static FibonacciGenerator BuildFibonacciGenerator(int n, string dictType)
+        {
+            var generator = new FibonacciGenerator();
+
+            if (dictType.Equals("SortedDictionary"))
+                generator._cache = new SortedDictionary<int, int>();
+            else
+                generator._cache = new Dictionary<int, int>();
+
+            generator.BuildFibNums(n);
+
+            return generator;
+        }
+
+
+        public static int FibonacciNumber(int n)
+        {
+            var a = 0;
+            var b = 1;
+
+            for (var i = 0; i < n; i++)
+            {
+                var tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
+        }
+
+        private int FibValue(int key)
+        {
+            if (!_cache.ContainsKey(key))
+            {
+                var value = FibonacciNumber(key);
+                _cache[key] = value;
+            }
+
+            return _cache[key];
+        }
+
+        private void BuildFibNums(int n)
+        {
+            Nums = new List<int>();
+            for (var i = 0; i < n; i++) Nums.Add(FibValue(i));
+        }
+    }
+}
---
+++ CH21Tests/FibonacciGeneratorTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH21Tests
+{
+    [TestClass]
+    public class FibonacciGeneratorTest
+    {
+        private readonly List<int> First21Fibs = new List<int>
+        {
+            0,
+            1,
+            1,
+            2,
+            3,
+            5,
+            8,
+            13,
+            21,
+            34,
+            55,
+            89,
+            144,
+            233,
+            377,
+            610,
+            987,
+            1597,
+            2584,
+            4181,
+            6765
+        };
+
+        private static List<int> GetFibNums(int n)
+        {
+            var nums = new List<int>();
+            for (var i = 0; i < n; i++) nums.Add(FibonacciGenerator.FibonacciNumber(i));
+            return nums;
+        }
+
+        [TestMethod]
+        public void GetFibNumsTest()
+        {
+            var fibs = GetFibNums(21);
+            CollectionAssert.AreEqual(First21Fibs, fibs);
+        }
+
+        [TestMethod]
+        public void FibonacciSortedDictGeneratorTest()
+        {
+            var gen = FibonacciGenerator.BuildFibonacciGenerator(21, "SortedDictionary");
+            CollectionAssert.AreEqual(First21Fibs, gen.Nums);
+        }
+
+        [TestMethod]
+        public void FibonacciDictGeneratorTest()
+        {
+            var gen = FibonacciGenerator.BuildFibonacciGenerator(21, "Dictionary");
+            CollectionAssert.AreEqual(First21Fibs, gen.Nums);
+        }
+    }
+}
