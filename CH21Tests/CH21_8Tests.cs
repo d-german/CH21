801b31fa053a2f5f0218e9e04ab81952f35aa59b
d-german <facmet107@gmail.com>
2018-10-15 18:48:32 -0500

    made some changes

--- CH21Tests/CH21_8Tests.cs
+++ CH21Tests/CH21_8Tests.cs
@@ -9,38 +9,60 @@ namespace CH21Tests
     {
         public delegate bool NumberPredicate(int number);
 
-        private int[] _numbers = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       
+        private readonly NumberPredicate _evenPredicate = EvenPredicate2;
+
+        private static bool EvenPredicate2(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        private readonly NumberPredicate _evenPredicate2 = delegate (int number) { return number % 2 == 0; };
+        private readonly NumberPredicate _evenPredicate3 = (int number) => { return number % 2 == 0; };
+        private readonly NumberPredicate _evenPredicate4 = number => number % 2 == 0;
+        private readonly NumberPredicate _evenPredicate5 = n => n % 2 == 0;
+
+        private readonly int[] _numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         private List<int> FilterArray(IEnumerable<int> array, NumberPredicate predicate)
         {
             var result = new List<int>();
             foreach (var num in array)
-            {
-                if (predicate(num)) result.Add(num);
-            }
+                if (predicate(num))
+                    result.Add(num);
 
             return result;
         }
 
-        private List<int> FilterArray(IEnumerable<int> array, Func<int, bool> predicate)
+        private List<int> FilterArrayFunc(IEnumerable<int> array, Func<int, bool> predicate)
         {
             var result = new List<int>();
             foreach (var num in array)
-            {
-                if (predicate(num)) result.Add(num);
-            }
+                if (predicate(num))
+                    result.Add(num);
 
             return result;
         }
 
-
-        NumberPredicate _evenPredicate = number => number % 2 == 0;
-
         [TestMethod]
-        public void TestMethod1()
+        public void EvenNumberTest()
         {
             var evenNumbers = FilterArray(_numbers, _evenPredicate);
+            CollectionAssert.AreEqual(new[] {2, 4, 6, 8, 10}, evenNumbers);
+        }
 
+        [TestMethod]
+        public void OddNumberTest()
+        {
+            var oddNumbers = FilterArrayFunc(_numbers, n => n % 2 == 1);
+            CollectionAssert.AreEqual(new[] {1, 3, 5, 7, 9}, oddNumbers);
+        }
+
+        [TestMethod]
+        public void NumbersOver5Test()
+        {
+            var oddNumbers = FilterArrayFunc(_numbers, n => n > 5);
+            CollectionAssert.AreEqual(new[] {6, 7, 8, 9, 10}, oddNumbers);
         }
     }
 }
