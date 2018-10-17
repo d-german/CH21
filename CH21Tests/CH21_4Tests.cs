749f2c032b572a6b989bcc6dd09f881007012220
d-german <facmet107@gmail.com>
2018-10-16 20:18:13 -0500

    made some changes

--- CH21Tests/CH21_4Tests.cs
+++ CH21Tests/CH21_4Tests.cs
@@ -49,15 +49,27 @@ namespace CH21Tests
             d["Two"] = 22; // updates dictionary because "two" is now present
 
             var buf = new StringBuilder();
-            foreach (var kv in d) buf.Append($"{kv.Key}; {kv.Value} ");
+            foreach (var kv in d)
+            {
+                buf.Append($"{kv.Key}; {kv.Value} ");
+            }
+
             Assert.AreEqual("One; 1 Two; 22 Three; 3 ", buf.ToString());
 
             buf.Clear();
-            foreach (var s in d.Keys) buf.Append(s);
+            foreach (var s in d.Keys)
+            {
+                buf.Append(s);
+            }
+
             Assert.AreEqual("OneTwoThree", buf.ToString());
 
             buf.Clear();
-            foreach (var v in d.Values) buf.Append(v);
+            foreach (var v in d.Values)
+            {
+                buf.Append(v);
+            }
+
             Assert.AreEqual("1223", buf.ToString());
         }
 
