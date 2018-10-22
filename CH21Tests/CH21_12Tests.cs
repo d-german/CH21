20e67469e64eebfc9c2a05502988dd27c08b8a2a
d-german <facmet107@gmail.com>
2018-10-21 20:13:41 -0500

    Made more changes

---
+++ CH21Tests/CH21_12Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CH21Tests
+{
+    [TestClass]
+    public class CH21_12Tests
+    {
+        private static readonly int[] _values;
+        private const int Max = 10000000;
+        private static int[] GetRandomArray()
+        {
+            var random = new Random();
+            return Enumerable.Range(1, Max)
+                .Select(x => random.Next(1, Max))
+                .ToArray();
+        }
+
+       static CH21_12Tests()
+       {
+           _values = GetRandomArray();
+       }
+        [TestMethod]
+        public void MaxTest()
+        {
+            var max = _values.Max();
+        }
+
+        [TestMethod]
+        public void AvgTest()
+        {
+            var avg = _values.Average();
+        }
+
+        [TestMethod]
+        public void MaxAsParallelTest()
+        {
+            var max = _values.AsParallel().Max();
+        }
+
+        [TestMethod]
+        public void AvgAsParallelTest()
+        {
+            var avg = _values.AsParallel().Average();
+        }
+    }
+}
