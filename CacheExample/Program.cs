301420e9fdc12ee58fa45b02187cef1597019852
Damon German <damon.german@hyland.com>
2020-11-07 19:03:23 -0600

    made some changes

---
+++ CacheExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Program
+{
+    private static readonly IDictionary<int, int> Cache = new Dictionary<int, int>();
+
+    private static int ExpensiveCalculation(int value)
+    {
+        Thread.Sleep(2000);
+        return value * 50;
+    }
+
+    private static int GetValue(int key)
+    {
+        //implement
+        throw new NotImplementedException();
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("....");
+        Console.WriteLine(GetValue(5));
+        Console.WriteLine(GetValue(10));
+        Console.WriteLine(GetValue(5));
+        Console.WriteLine(GetValue(10));
+    }
+}
---
+++ LinqExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    private static readonly List<int> Values = new List<int> {3, 10, 6, 1, 4, 8, 2, 5, 9, 7};
+
+    private static void Main(string[] args)
+    {
+        // Write the LINQ Query that filters values > 2 and sorts them in numeric order.
+        // Then write the values to the command line using foreach
+    }
+}
