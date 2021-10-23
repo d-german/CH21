e4123f94a5686adc03f2d308817979f6875628c7
Damon German <damon.german@hyland.com>
2021-10-23 12:55:42 -0500

    made some changes

---
+++ CH21Tests.backup/CH21_04Tests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CH21Tests
+{
+    [TestFixture]
+    public class CH21_04Tests
+    {
+        [Test]
+        public void TestAddUpdateDictTest()
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("One", 1);
+            dictionary["Two"] = 2; // adds to dictionary because "two" not already present
+            dictionary["Two"] = 22; // updates dictionary because "two" is now present
+            dictionary["Three"] = 3;
+
+            Assert.AreEqual(22, dictionary["Two"]);
+            Assert.IsTrue(dictionary.ContainsKey("One")); // true (fast operation)
+            Assert.IsTrue(dictionary.ContainsValue(3)); // true (slow operation)
+        }
+
+        [Test]
+        public void TestEnumerateDictTest()
+        {
+            var dictionary = new Dictionary<string, int>
+            {
+                {"One", 1},
+                {"Two", 2},
+                //{"Two", 22 },
+                {"Three", 3}
+            };
+
+            dictionary["Two"] = 22; // updates dictionary because "two" is now present
+
+            var buf = new StringBuilder();
+            foreach (var kv in dictionary)
+            {
+                buf.Append($"{kv.Key}; {kv.Value} ");
+            }
+
+            Assert.AreEqual("One; 1 Two; 22 Three; 3 ", buf.ToString());
+
+            buf.Clear();
+            foreach (var str in dictionary.Keys)
+            {
+                buf.Append(str);
+            }
+
+            Assert.AreEqual("OneTwoThree", buf.ToString());
+
+            buf.Clear();
+            foreach (var value in dictionary.Values)
+            {
+                buf.Append(value);
+            }
+
+            Assert.AreEqual("1223", buf.ToString());
+        }
+
+        [Test]
+        public void HashToIndexTest()
+        {
+            var hash = new string[8];
+            hash.Insert("A_One");
+            hash.Insert("B_Two");
+            hash.Insert("C_Three");
+            hash.Insert("D_Four");
+            hash.Insert("E_Five");
+            hash.Insert("F_Six");
+            hash.Insert("G_Seven");
+            hash.Insert("H_Eight");
+            hash.Insert("I_Nine");
+            hash.Insert("J_Ten");
+            hash.Insert("K_Eleven");
+
+            _ = hash
+                .OrderBy(i => i)
+                .Select(value =>
+                {
+                    Console.WriteLine(value ?? "-");
+                    return value;
+                })
+                .Where(n => !string.IsNullOrEmpty(n))
+                .Select(n => n)
+                .OrderBy(n => n)
+                .ToList();
+        }
+    }
+}
---
+++ CH21Tests.backup/EagerLazyTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using static CH21Tests.Extensions;
+
+namespace CH21Tests
+{
+    [TestFixture]
+    public class EagerLazyTests
+    {
+        private static IEnumerable<string> UVtoIR()
+        {
+            yield return "violet";
+            yield return "blue";
+            yield return "cyan";
+            yield return "green";
+            yield return "yellow";
+            yield return "orange";
+            yield return "red";
+        }
+
+        private static IEnumerable<string> IRtoUV()
+        {
+            string[] colors = {"violet", "blue", "cyan", "green", "yellow", "orange", "red"};
+
+            for (var i = colors.Length - 1; i >= 0; i--)
+            {
+                yield return colors[i];
+            }
+        }
+
+        private Stopwatch _stopWatch;
+
+        [SetUp]
+        public void Init()
+        {
+            _stopWatch = new Stopwatch();
+            _stopWatch.Start();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _stopWatch.Stop();
+            Console.WriteLine($"{_stopWatch.ElapsedMilliseconds} ms");
+        }
+
+        [Test]
+        public void GetSpectrum()
+        {
+            foreach (var color in UVtoIR())
+            {
+                Console.WriteLine($"{color}");
+            }
+
+            Console.WriteLine("--------------");
+
+            foreach (var color in IRtoUV())
+            {
+                Console.WriteLine($"{color}");
+            }
+        }
+
+        [Test]
+        public void EagerExecution()
+        {
+            var results = GetRangeEager(0, 500).FilterEager(i => i < 25).TakeEager(4);
+            Assert.AreEqual("0 1 2 3", results.Display());
+        }
+
+        [Test]
+        public void LazyExecution()
+        {
+            var results = GetRangeLazy(0, 500).FilterLazy(i => i < 25).TakeLazy(4);
+            Assert.AreEqual("0 1 2 3", results.Display());
+        }
+    }
+}
